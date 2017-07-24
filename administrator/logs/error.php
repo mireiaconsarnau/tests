439bc4dce792b07b895ac237e69ed7b7d627da88
mireiaconsarnau <mireiaconsarnau@gmail.com>
2017-07-24 11:15:00 +0200

    primer commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-07-19 15:44:08 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-07-19T15:44:07+00:00	INFO 127.0.0.1	joomlafailure	Contrasenya incorrecta
